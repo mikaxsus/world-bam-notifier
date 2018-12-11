f7f31f44ecc4666c80349ad07518fd6bd8353499
quality-reaper <45637893+mikaxsus@users.noreply.github.com>
2018-12-11 14:29:09 +0100

    fix v2

--- index.js
+++ index.js
@@ -9,27 +9,15 @@ module.exports = function WorldBamNotifier(dispatch) {
 		99: 	{msg: "Found Divine Reaver!"}
 	};
 
-	// templateId : huntingZoneId
+	//templateId : huntingZoneId
 	const bossId = [
-		[35, 38], 	// Nyxarras
-		[9050, 52], // Yunaras Snaggletooth
-		[7011, 51], // Linyphi
-		[33, 57], 	// Betsael
-		[5011, 4], 	// Tempest Kanash
-		[99, 10], 	// Divine Reaver
+		[35, 38], 	//Nyxarras
+		[9050, 52], //Yunaras Snaggletooth
+		[7011, 51], //Linyphi
+		[33, 57], 	//Betsael
+		[5011, 4], 	//Tempest Kanash
+		[99, 10], 	//Divine Reaver
 	]; 
-	
-/*
-	// 42 Blue Shiny Text, 31 Normal Text
-	const msg-chat = {unk1: 42, unk2: 0, unk3: 27, message: ""}; 	
-	const msg-whisp = { channel: 7, authorName: "", message: ""};
-	
-	const systemMessage = msg => {
-		msgObject.message = whisperObj.message = msg;
-		toClient("S_CHAT", 1, whisperObj);
-		toClient("S_DUNGEON_EVENT_MESSAGE", 1, msgObject);
-  };
-*/
 
 	function sendMsg(msg) {
 		dispatch.toServer('C_CHAT', 1, {
