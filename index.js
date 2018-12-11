350053920d8a75e0edbe529f4716a7eddd353ba0
quality-reaper <45637893+mikaxsus@users.noreply.github.com>
2018-12-11 14:18:41 +0100

    Fixed nextgen issues

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
 module.exports = function WorldBamNotifier(dispatch) {
-  const bossName = {
-    "35": "Nyxarras",
-    "9050": "Yunaras Snaggletooth",
-    "7011": "Linyphi",
-    "33": "Betsael",
-    "5011": "Tempest Kanash",
-    "99": "Divine Reaver"
-  }, toClient = dispatch.toClient.bind(dispatch);
+	//add your own by finding templateId and matching it to huntingZoneId
+	const bossName = {
+		35: 	{msg: "Found Nyxarras!"},
+		9050: 	{msg: "Found Yunaras Snaggletooth!"},
+		7011: 	{msg: "Found Linyphi!"},
+		33: 	{msg: "Found Betsael!"},
+		5011: 	{msg: "Found Tempest Kanash!"},
+		99: 	{msg: "Found Divine Reaver!"}
+	};
 
-  // templateId : huntingZoneId
-  const bossId = [
-    [35, 38], // Nyxarras
-    [9050, 52], // Yunaras Snaggletooth
-    [7011, 51], // Linyphi
-    [33, 57], // Betsael
-    [5011, 4], // Tempest Kanash
-    [99, 10], // Divine Reaver
-  ], msgObject = {
-    unk1: 42, // 42 Blue Shiny Text, 31 Normal Text
-    unk2: 0, unk3: 27, message: ""
-  }, whisperObj = {
-    channel: 7, authorName: "",
-    message: ""
+	// templateId : huntingZoneId
+	const bossId = [
+		[35, 38], 	// Nyxarras
+		[9050, 52], // Yunaras Snaggletooth
+		[7011, 51], // Linyphi
+		[33, 57], 	// Betsael
+		[5011, 4], 	// Tempest Kanash
+		[99, 10], 	// Divine Reaver
+	]; 
+	
+/*
+	// 42 Blue Shiny Text, 31 Normal Text
+	const msg-chat = {unk1: 42, unk2: 0, unk3: 27, message: ""}; 	
+	const msg-whisp = { channel: 7, authorName: "", message: ""};
+	
+	const systemMessage = msg => {
+		msgObject.message = whisperObj.message = msg;
+		toClient("S_CHAT", 1, whisperObj);
+		toClient("S_DUNGEON_EVENT_MESSAGE", 1, msgObject);
   };
+*/
 
-  const systemMessage = msg => {
-    msgObject.message = whisperObj.message = msg;
-    toClient("S_CHAT", 1, whisperObj);
-    toClient("S_DUNGEON_EVENT_MESSAGE", 1, msgObject);
-  };
+	function sendMsg(msg) {
+		dispatch.toServer('C_CHAT', 1, {
+			channel: 21,
+			message: msg
+		});
+	}
 
-  dispatch.hook("S_SPAWN_NPC", 5, event => {
-    const { templateId, huntingZoneId } = event;
-    for (let i = 0, len = bossId.length; i < len; ++i) {
-      const boss = bossId[i];
-      if (templateId === boss[0] && huntingZoneId === boss[1]) {
-        systemMessage("BAM Found: " + bossName[templateId]);
-      }
-    }
-  });
+	dispatch.hook("S_SPAWN_NPC", 5, event => {
+		const { templateId, huntingZoneId } = event;
+		for (let i = 0, len = bossId.length; i < len; ++i) {
+			const boss = bossId[i];
+			if (templateId === boss[0] && huntingZoneId === boss[1]) {
+				sendMsg(bossName[0].msg);
+			}
+		}
+	});
 };
